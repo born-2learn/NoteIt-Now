30528127f824feb79333a0fcfeec346255027ec6
Syed Farhan <42010556+born-2learn@users.noreply.github.com>
2020-03-07 06:52:14 +0530

    Update settings.py

--- noteitnow/settings.py
+++ noteitnow/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "hello12345678"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
