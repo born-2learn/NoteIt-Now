b4a9da1b2eeb919b251645bc979258399024d8cd
Syed Farhan <42010556+born-2learn@users.noreply.github.com>
2020-03-07 07:13:20 +0530

    Update wsgi.py

--- noteitnow/wsgi.py
+++ noteitnow/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notekeeper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'noteitnow.settings')
 
 application = get_wsgi_application()
